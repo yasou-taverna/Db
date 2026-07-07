62ba3a9ec70163ed659056af2e34a841cdd400d3
Yasou Taverna <yasoutavern@gmail.com>
2026-07-07 12:45:39 -0400

    Update API_URL to new endpoint

--- booking/js/booking-api.js
+++ booking/js/booking-api.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://script.google.com/macros/s/AKfycbySbpecrxWDu0NZVxRI-19B8DsQqJWafejcQ7GU_1KTS9KDDHi_8DGY3WjUuUdCS4w87g/exec';
+export const API_URL = 'https://script.google.com/macros/s/AKfycbyR8vPh0M-RsbkLoVFlPWSK9GqZL38v0OC73vg4KkldhgPBf3XJrMSGz7XCpNR1ZWVW/exec';
 
 export async function postReservation(reservation) {
   const response = await fetch(API_URL, {
